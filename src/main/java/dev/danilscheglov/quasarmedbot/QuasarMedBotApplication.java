5449b7511f33ec1c26d8e0ffab773933289480f7
Danil Scheglov <scheglovofficial@gmail.com>
2025-07-02 19:41:22 +0300

    init: create project

---
+++ src/main/java/dev/danilscheglov/quasarmedbot/QuasarMedBotApplication.java
@@ -0,0 +1,13 @@
+package dev.danilscheglov.quasarmedbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuasarMedBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuasarMedBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/danilscheglov/quasarmedbot/QuasarMedBotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.danilscheglov.quasarmedbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuasarMedBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
