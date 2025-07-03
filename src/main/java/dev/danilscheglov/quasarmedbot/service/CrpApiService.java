86f4954eec463a0f7d19ec7558ec1dc53da8256b
Danil Scheglov <scheglovofficial@gmail.com>
2025-07-03 20:38:45 +0300

    docs(crp): added Javadoc to CrpApiService

--- src/main/java/dev/danilscheglov/quasarmedbot/service/CrpApiService.java
+++ src/main/java/dev/danilscheglov/quasarmedbot/service/CrpApiService.java
@@ -2,10 +2,25 @@ package dev.danilscheglov.quasarmedbot.service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Класс сервиса, ответственный за взаимодействие с API ЦРП для поиска записей пользователей.
+ * Текущая реализация является заглушкой и возвращает фиксированный тестовый идентификатор.
+ * Предназначен для интеграции с внешней системой ЦРП в производственной среде.
+ *
+ * @author Данил Щеглов (Danil Scheglov)
+ * @since 2025-07-03
+ */
 @Service
 public class CrpApiService {
 
-    public String searchByFioAndBirthdate(String lastName, String firstName, String middleName, String birthdate) {
+    /**
+     * @param lastName   Фамилия пользователя
+     * @param firstName  Имя пользователя
+     * @param middleName Отчество пользователя
+     * @param birthdate  Дата рождения пользователя в формате, подходящем для обработки API
+     * @return Строка, представляющая идентификатор результата поиска (например, "test-id-123").
+     */
+    public String search(String lastName, String firstName, String middleName, String birthdate) {
         return "test-id-123";
     }
 }
